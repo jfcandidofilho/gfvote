0abd85eb1f989c8717f589eb2bb5f4eec1e988eb
J. F. Candido Filho <jfcandidofilho@gmail.com>
2020-07-07 00:26:16 -0300

    MINOR Add vote check, code state and toopp function
    Summary
    
    * change "my_votes" to "vote_list"
    * add "STATE" global variable
    * add toopp function
    * add a vote check option in DEV state
    
    The code currently supports three types of states: development (DEV),
    debug (DEBUG) and production (PROD). To change this state, simply
    change the value of "STATE" global variable.
    
    In this commit, in DEV state, the code outputs the selected vote in
    "vote_list" global variable. It is useful to check if the selected vote
    is the desired vote for a given poll.
    
    Note that these variable names can change. Like "my_votes" changed to
    "vote_list".
    
    The "toopp" function sums every option of every previously considered
    poll to calculate the index of the options of current poll. This is
    necessary because all the option of every and each poll are packed
    together.

--- gfvote.js
+++ gfvote.js
@@ -1,3 +1,11 @@
+/*
+    Selects the state of the code running
+    "DEV" -> tests for development
+    "DEBUGG" -> Debugging the error
+    "PROD" -> Production code
+*/
+var STATE = "DEV";
+
 // Sets number of options a given poll has, polls separated by comma
 var options_per_category = [
 
@@ -5,9 +13,46 @@ var options_per_category = [
 
 ];
 
-// Your vote for a poll, each vote separated by a comma
-var my_votes = [
+// Your vote for each poll, each vote for each poll separated by a comma
+var vote_list = [
 
     0, 6, 5, 1, 3, 5, 5, 3, 3, 3, 6, 0, 2, 6, 1, 3, 5, 0, 6, 6, 3, 1, 3, 4, 2, 6, 3
 
 ];
+
+// Returns the sum of the number of options of each previous considered poll.
+// "toopp" stands for "total options of previous polls"
+function toopp( option_list, current_index ){
+
+    var count = 0;
+
+    for( var i = 0; i < current_index; i++ ){
+
+        count += option_list[ i ];
+
+    }
+
+    return count;
+
+}
+
+// Select each poll to vote for
+for( var poll = 0; poll < options_per_category.length; poll++ ){
+
+    // Select the proper element in a list of elements to vote for
+    var vote = toopp( options_per_category, poll ) + vote_list[ poll ];
+
+    // Verifies the vote selected
+    if( STATE == "DEV" ) console.log( 
+        
+        "POLL: " + (poll + 1),
+        
+        "VOTE: " + document.querySelectorAll(
+            
+            ".appsMaterialWizToggleRadiogroupEl"
+            
+        )[ vote ].dataset.value
+        
+    );
+
+}
