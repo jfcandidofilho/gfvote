00dddf940cc8d196e1d79d7ae0bf86023a071450
J. F. Candido Filho <jfcandidofilho@gmail.com>
2020-07-06 23:56:33 -0300

    MINOR Add polling selection and vote specification
    The code currently receives the number of polls and the vote for each
    poll. It doesn't do anything with it but it already masks the intetion.

---
+++ gfvote.js
@@ -0,0 +1,13 @@
+// Sets number of options a given poll has, polls separated by comma
+var options_per_category = [
+
+    6, 7, 8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7
+
+];
+
+// Your vote for a poll, each vote separated by a comma
+var my_votes = [
+
+    0, 6, 5, 1, 3, 5, 5, 3, 3, 3, 6, 0, 2, 6, 1, 3, 5, 0, 6, 6, 3, 1, 3, 4, 2, 6, 3
+
+];
